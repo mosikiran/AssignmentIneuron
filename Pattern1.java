668b3424118beca76e0e3b4b21eaebfad2b4b266
mosikiran <94050284+mosikiran@users.noreply.github.com>
2022-12-02 22:52:33 +0530

    Add files via upload
    Assignment-1

---
+++ Pattern1.java
@@ -0,0 +1,24 @@
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 14;
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j < num; j++) {
+
+				if (i == 0 || j == 0 || i == (num - 1) || j == (num - 1) || (i + j <= ((num - 1) / 2))
+						|| (j - i >= ((num - 1) / 2)))
+
+				{
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ Pattern4.java
@@ -0,0 +1,18 @@
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 4;
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j < num; j++) {
+
+				System.out.print(i + 1);
+
+			}
+			System.out.println();
+		}
+
+	}
+
+}
